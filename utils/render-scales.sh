622573593d659c01db8ede5fe7e46b58ca24b1e8
xycarto <xycarto@gmail.com>
2023-03-22 15:18:11 +1300

    future processing

--- utils/render-scales.sh
+++ utils/render-scales.sh
@@ -10,3 +10,7 @@ for scl in ${scales[@]}
 do
     python3 utils/image-export.py $QGIS_PROJ $scl $PROCESSORS
 done
+
+# python3 utils/create-vrts.py 32000000,16000000,8000000,4000000,2000000,1000000,500000,250000,100000,50000
+# python3 utils/raster-tiler.py 8
+# python3 utils/clean-dirs.py
